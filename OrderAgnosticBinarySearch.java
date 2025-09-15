e4b5e15dee62d07926ef9ac82a808409338cb867
ht0241529-tiwari <ht0241529@gmail.com>
2025-09-15 12:36:28 +0000

    done

---
+++ OrderAgnosticBinarySearch.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+
+public class OrderAgnosticBinarySearch {
+    public static void main(String[] args) {
+   int [] arr={1,2,3,4,5,6};
+   int target=5;
+        int ans=oabs(arr, target);
+        System.out.println(ans);        
+    }
+    static int oabs(int [] arr,int target){
+        int start=0;
+        int end=arr.length-1;
+        boolean isasc=arr[start]<arr[end];
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(arr[mid]==target){
+                return mid;
+            }
+            if(isasc){
+                if(arr[mid]>target){
+                    end=mid-1;
+                }
+                else{
+                    start=mid+1;
+                }
+            }
+            else{
+                if(arr[mid]<target){
+                    end=mid-1;
+                }
+                else{
+                    start=mid+1;
+                }
+            }
+        }
+        return -1;
+    }
+}
